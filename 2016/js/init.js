bbbf985c684d275acb0a48dd7c4fba27e6003dfd
carlos caimi <mkt@caimi.com.br>
2016-05-16 08:27:54 -0300

    site ftsl versao 2016

---
+++ 2016/js/init.js
@@ -0,0 +1,40 @@
+(function ($) {
+  $(function () {
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $("#footerContent").load("footer.html");
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$(function () { // wait for document ready
+
+  /*
+  SETA ALTURA DO DIV PARA O TAMANHO DA TELA DISPONIVEL
+  console.log(window.innerHeight, window.innerWidth);
+
+  $(".panel").each(function () {
+    $(this).height(window.innerHeight);
+  });*/
+
+  // init
+  var controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      triggerHook: 'onLeave'
+    }
+  });
+
+  // get all slides
+  var slides = document.querySelectorAll("section.panel");
+
+  // create scene for every slide
+  for (var i = 0; i < slides.length; i++) {
+    new ScrollMagic.Scene({
+        triggerElement: slides[i]
+      })
+      .setPin(slides[i])
+      //.addIndicators() // add indicators (requires plugin)
+      .addTo(controller);
+  }
+});
