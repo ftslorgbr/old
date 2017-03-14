d3cd79ccebb94a95f4c2afabcf9219a0ae2cea23
carlos caimi <carlos caimi>
2017-03-13 21:48:26 -0300

    2017 started

---
+++ 2017/js/init.js
@@ -0,0 +1,43 @@
+(function ($) {
+    $(function () {
+        $("#menu").load("menu.html", function () {
+            $('.button-collapse').sideNav();
+        });
+        $('.parallax').parallax();
+        $("#footerContent").load("footer.html");
+        $("#patrocinio").load("logo.html");
+        $("#patrocinioFooter").load("logo.html");
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$(function () { // wait for document ready
+    /*
+    SETA ALTURA DO DIV PARA O TAMANHO DA TELA DISPONIVEL
+    console.log(window.innerHeight, window.innerWidth);
+
+    $(".panel").each(function () {
+      $(this).height(window.innerHeight);
+    });*/
+
+    // init
+    /*
+    var controller = new ScrollMagic.Controller({
+      globalSceneOptions: {
+        triggerHook: 'onLeave'
+      }
+    });
+
+    // get all slides
+    var slides = document.querySelectorAll("section.panel2");
+
+    // create scene for every slide
+    for (var i = 0; i < slides.length; i++) {
+      new ScrollMagic.Scene({
+          triggerElement: slides[i]
+        })
+        .setPin(slides[i])
+        //.addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+    }*/
+});
