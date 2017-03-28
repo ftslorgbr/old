27871f9f45d917e8af0880d3c1ce2223f1eb1b35
carlos caimi <mkt@caimi.com.br>
2017-03-28 09:20:02 -0300

    proposta inicial 2017

--- 2017/js/init.js
+++ 2017/js/init.js
@@ -7,6 +7,7 @@
         $("#footerContent").load("footer.html");
         $("#patrocinio").load("logo.html");
         $("#patrocinioFooter").load("logo.html");
+        countdown(new Date("Sep 27, 2017 08:00:00").getTime());
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
@@ -41,3 +42,35 @@ $(function () { // wait for document ready
         .addTo(controller);
     }*/
 });
+
+function countdown(diaD){
+
+var countDownDate = new Date("Sep 27, 2017 08:00:00").getTime();
+countDownDate = diaD;
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("count").innerHTML = days + "d " + hours + "h "
+  + minutes + "m "; //+ seconds + "s ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+}
