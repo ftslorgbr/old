be00d6b364d3ba3bada59291ce0c3779295962d3
JonesBalada <vacahk7@gmail.com>
2017-07-25 22:26:37 -0300

    arquivo de teste

---
+++ grade/mobile.php
@@ -0,0 +1,14 @@
+[
+  {
+    "pid": "69",
+    "date": "2",
+    "time": "22",
+    "type": "2",
+    "place": "15",
+    "title": "HTML5/REST ",
+    "description": "restfull",
+    "authorId": "1953",
+    "authorName": "Juca Bala",
+    "curriculum": "comunidad curriculofd"
+  }
+]
